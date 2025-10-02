5d7409cd6d70152fbecfff1e4b1b2f61aa7af980
Iustin <contact@zup.digital>
2025-10-02 14:56:45 +0300

    last save

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className="footer-section">
-      <div className="section-container footer-container">
+      <div className="footer-container">
         <p className="footer-copyright">
           COPYRIGHT © {new Date().getFullYear()} ZUP! - ALL RIGHTS RESERVED.
         </p>
